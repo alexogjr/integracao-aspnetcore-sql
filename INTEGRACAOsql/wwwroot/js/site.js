e43cc36fdb83606f7dc48c25b7fdd4cfe04011e3
unknown <alex.sandro.junior.dev@gmail.com>
2025-01-22 23:09:28 -0300

    atualização no frontend

--- INTEGRACAOsql/wwwroot/js/site.js
+++ INTEGRACAOsql/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const term = $('body').terminal();
+
+github('jcubic/jquery.terminal');
